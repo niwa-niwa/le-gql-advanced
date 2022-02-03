961d8fb631b5e705a08f101cc4820006acefa2b6
niwa-niwa <urban.hang274@gmail.com>
2022-02-03 23:07:31 +0900

    implemented db for user and message

---
+++ src/db/message.ts
@@ -0,0 +1,9 @@
+import { Messages } from "../types";
+
+const messages: Messages = {
+  "1": { id: "1", text: "Hello, world!", userId: "1" },
+  "2": { id: "2", text: "from GraphQL and Apollo-Server.", userId: "2" },
+  "3": { id: "3", text: "Hello, world!", userId: "2" },
+};
+
+export default messages;
---
+++ src/db/user.ts
@@ -0,0 +1,8 @@
+import { Users } from "../types";
+
+const users: Users = {
+  "1": { id: "1", firstName: "fisher_man", lastName: "sow" },
+  "2": { id: "2", firstName: "fisher_woman", lastName: "mow" },
+};
+
+export default users;
